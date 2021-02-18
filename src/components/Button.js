39dda0a42b6130d8d67614b04d8bace3e06884a1
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2021-02-18 14:09:00 +0100

    Navbar is looking good and resizable

--- src/components/Button.js
+++ src/components/Button.js
@@ -20,10 +20,14 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-      <Link to='/sign-up' className='btn-mobile'>
-          <button>
-              {children}
-          </button>
-      </Link>
-  )
+    <Link to="/sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 };
