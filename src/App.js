48518926ec4e5518c0f4ce56f3a50530890c7985
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2021-02-19 16:20:37 +0100

    Created files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from './components/pages/Home'
+import Home from "./components/pages/Home";
+import Pokemons from "./components/pages/Pokemons";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         <div>
           <Navbar></Navbar>
           <Switch>
-            <Route path="/" exact  component={Home}/>
+            <Route path="/" exact component={Home} />
+            <Route path="/pokemons" component={Pokemons} />
           </Switch>
         </div>
       </Router>
---
+++ src/components/pages/Pokemons.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../App.css";
+
+export default function Pokemons() {
+  return <h1 className="pokemons">POKEMONS</h1>;
+}
