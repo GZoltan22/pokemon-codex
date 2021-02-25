988a1e42e6f26585a71567398bdaeb0ec20ff371
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2021-02-25 12:19:50 +0100

    made a pokemondetails page

---
+++ src/components/pages/PokemonDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PokemonDetails() {
+    return (
+        <div>
+            <p>PokemonDetails page</p>
+        </div>
+    )
+}
+
+export default PokemonDetails;
