521b71bea21b59e7ab9e3fffde4430e3b9561c0e
GZoltan22 <55540615+GZoltan22@users.noreply.github.com>
2021-03-03 10:55:28 +0100

    Thinking about the stricture of the PokemonsPage

---
+++ src/components/PokemonCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PokemonCards({pokemons}) {
+    console.log(pokemons);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PokemonCards
--- src/components/pages/Pokemons.js
+++ src/components/pages/Pokemons.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
 import HeroSection from "../HeroSection";
+import PokemonCards from "../PokemonCards";
 
-export default function Pokemons() {
+export default function Pokemons({ pokemons }) {
   return (
     <>
       <HeroSection />
+      <PokemonCards pokemons={pokemons} />
       <Footer />
     </>
   );
